da5a489eb9245c09eb0246c2e619ac77e98c3f56
Zou Jiyun <zoujiyun@tinman.cn>
2021-09-15 19:46:48 +0800

    🌞

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ const Context = createContext(null)
 
 function App() {
 
-  const [msg, setMsg] = useState('123')
+  const [user, setUser] = useState({
+    name: 'liming',
+    msg: 'iam a doctor'
+  })
 
   return (
-    <Context.Provider value={{msg, setMsg}}>
+    <Context.Provider value={{ user, setUser }}>
       <One />
       <Two />
       <Three />
@@ -24,20 +27,47 @@ const One = () => (
 );
 const Two = () => (
   <section>
-    TWO <ModifyMsg />
+    TWO <Wrapper />
   </section>
 );
 const Three = () => <section>THREE</section>;
 
-const Msg = () => {
-  const {msg} = useContext(Context)
-  return <>{msg}</>
+
+const reducer = (state, { type, payload}) => {
+  switch (type) {
+    case 'updateUser':
+      return {
+        ...state,
+        ...payload
+      }
+    default:
+      return state
+  }
 }
 
-const ModifyMsg = () => {
-  const {setMsg} = useContext(Context)
+const Msg = () => {
+  const {user} = useContext(Context)
+  return (
+    <>
+      {user.name} + {user.msg}
+    </>
+  );
+}
+
+const ModifyMsg = ({dispatch, state}) => {
   const onChange = (e) => {
-    setMsg(e.target.value)
+    dispatch({ type: "updateUser", payload: { msg: e.target.value } });
   }
-  return <input onChange={onChange} />
+  return (
+    <input onChange={onChange} value={state.msg} placeholder={state.msg} />
+  );
+}
+
+const Wrapper = () => {
+  const { user, setUser } = useContext(Context);
+  const dispatch = (action) => {
+    setUser(reducer(user, action))
+  }
+
+  return <ModifyMsg dispatch={dispatch}  state={user} />
 }
