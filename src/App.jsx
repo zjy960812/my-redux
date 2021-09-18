f159b8b0e0c9b52ed9ebd76c2521d493760f2ee2
Zou Jiyun <1120957260@qq.com>
2021-09-18 16:22:04 +0800

    利用connect减少render

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import React, { useState, createContext, useContext } from 'react'
+import React, { useState, createContext, useContext, useEffect } from 'react'
 
 const Context = createContext(null)
 
+const store = {
+  state: {
+    name: "liming",
+    msg: "iam a doctor",
+  },
+  setState(newState) {
+    store.state = newState
+    store.listeners.forEach(fn => {fn()})
+  },
+  listeners: [],
+  subscribe(fn) {
+    store.listeners.push(fn)
+    return () => {
+      store.listeners = store.listeners.filter(f => f !== fn)
+    }
+  }
+};
+
+const connect = (Component) => {
+  return (props) => {
+    const { state: user, setState: setUser, subscribe } = useContext(Context);
+    const [, update] = useState({})
+    useEffect(() =>{
+      subscribe(() => update({}))
+    }, [])
+    const dispatch = (action) => {
+      setUser(reducer(user, action));
+    };
+
+    return <Component {...props} dispatch={dispatch} state={user} />;
+  };
+};
+
 function App() {
-
-  const [user, setUser] = useState({
-    name: 'liming',
-    msg: 'iam a doctor'
-  })
-
   return (
-    <Context.Provider value={{ user, setUser }}>
+    <Context.Provider value={store}>
       <One />
       <Two />
       <Three />
@@ -20,16 +47,18 @@ function App() {
 
 export default App
 
-const One = () => (
-  <section>
+const One = () => {
+  console.log('render 1')
+  return (<section>
     ONE <Msg />
-  </section>
-);
-const Two = () => (
-  <section>
+  </section>)
+};
+const Two = () => {
+  console.log('render 2')
+  return <section>
     TWO <ModifyMsg label={'x'} />
   </section>
-);
+}
 const Three = () => <section>THREE</section>;
 
 
@@ -45,14 +74,14 @@ const reducer = (state, { type, payload}) => {
   }
 }
 
-const Msg = () => {
-  const {user} = useContext(Context)
+const Msg = connect(() => {
+  const { state: user } = useContext(Context);
   return (
     <>
       {user.name} + {user.msg}
     </>
   );
-}
+});
 
 const _ModifyMsg = ({dispatch, state, label}) => {
   const onChange = (e) => {
@@ -64,17 +93,6 @@ const _ModifyMsg = ({dispatch, state, label}) => {
       <input onChange={onChange} value={state.msg} placeholder={state.msg} />
     </>
   );
-}
-
-const connect = (Component) => {
-  return (props) => {
-    const { user, setUser } = useContext(Context);
-    const dispatch = (action) => {
-      setUser(reducer(user, action));
-    };
-
-    return <Component {...props} dispatch={dispatch} state={user} />;
-  };
 }
 
 const ModifyMsg = connect(_ModifyMsg)
